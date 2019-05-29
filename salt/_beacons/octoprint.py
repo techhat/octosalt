7bfad3763434e0969df4f32686be9c2ef4e0a1b0
Joseph Hall <perlhoser@gmail.com>
2019-05-28 21:41:58 -0600

    Add OctoPrint beacon

---
+++ salt/_beacons/octoprint.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+'''
+Beacon for OctoPrint
+'''
+
+# Import python libs
+from __future__ import absolute_import, generators, print_function, with_statement, unicode_literals
+import json
+import logging
+
+# Import salt libs
+import salt.utils.http
+
+__virtualname__ = 'octoprint'
+
+log = logging.getLogger(__name__)
+
+
+def __virtual__():
+    '''
+    Only load the module if OctoPrint is installed
+    '''
+    if __opts__['pillar']['proxy']['proxytype'] == 'octoprint':
+        return True
+    return (False, 'The OctoPrint modules cannot be loaded: proxy is not configured.')
+
+
+def validate(config):
+    '''
+    Validate configuration
+    '''
+    return True, 'Valid beacon configuration'
+
+
+def beacon(config):
+    '''
+    Return printer status
+    '''
+    url = '{0}/api/printer'.format(__opts__['pillar']['proxy']['url'])
+    data = salt.utils.http.query(
+        url,
+        'GET',
+        header_dict={'X-Api-Key': __opts__['pillar']['proxy']['apikey']},
+        opts=__opts__,
+        decode=True,
+        decode_type='json',
+    )['dict']
+    return [data]
